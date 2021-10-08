edf97a3c84b3b03e08dc0957dd952eb989fc7d7b
Kathryn Wheeler <kiwheel@bu.edu>
2021-10-07 21:38:34 -0400

    Fixed inconsistency

--- uncertaintyAnalysisHindcasts.R
+++ uncertaintyAnalysisHindcasts.R
@@ -83,65 +83,68 @@ uncertaintyAnalysisHindcast <- function(filePattern,pdfName,b,tID){
         print(fileName)
         tle <- strsplit(fileName,"_")[[1]][1:2]
         calibrationSiteName <- strsplit(fileName,"_")[[1]][1]
-        
-        load(fileName) #Load Model Output 
-        
-        out.mat <- data.frame(as.matrix(out.burn$param))
-        
-        b0 <- out.mat$b0
-        b1 <- out.mat$b1
-        b2 <- out.mat$b2
-        if(b=="b3"){
-          b3 <- out.mat$b3
-          b4 <- rep(0,length(b3))
-          b5 <- rep(0,length(b3))
-        }else if(b=="b4"){
-          b4 <- out.mat$b4
-          b3 <- rep(0,length(b4))
-          b5 <- rep(0,length(b4))
-        }else if(b=="b5"){
-          b5 <- out.mat$b5
-          b3 <- rep(0,length(b5))
-          b4 <- rep(0,length(b5))
-        }
-        
-        NT <- length(dataFinal$TairMu[,1])
-        
-        Nmc <- 1000
-        prow = sample.int(nrow(out.mat),Nmc,replace=TRUE)
-        
-        initialXs <- rbeta(prow,dataFinal$x1.a[1],dataFinal$x1.b[1])
-        
-        print(mean(initialXs))
-        
-        days <- seq(1,NT)
-        ysDet <- forecastStep(IC=mean(initialXs),b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
-                              n=1,NT=length(days),Tair=dataFinal$TairMu[,1],D=dataFinal$D[,1]) #Dependent on IC, but independent for >=1
-        ysParam <- forecastStep(IC=mean(initialXs),b0=b0[prow],b1=b1[prow],b2=b2[prow],b3=b3[prow],b4=b4[prow],b5=b5[prow],
-                                n=Nmc,NT=NT,Tair=dataFinal$TairMu[,1],D=dataFinal$D[,1])
-        ysProc <- forecastStep(IC=mean(initialXs),b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
-                               n=Nmc,NT=NT,Q=sqrt(1/out.mat$p.proc[prow]),Tair=dataFinal$TairMu[,1],D=dataFinal$D[,1])
-        ysIC <- forecastStep(IC=initialXs,b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
-                             n=length(initialXs),NT=NT,Tair=dataFinal$TairMu[,1],D=dataFinal$D[,1])
-        
-        for(i in 2:dataFinal$N){
-          initialXs <- rbeta(prow,dataFinal$x1.a[i],dataFinal$x1.b[i])
-          
-          ysDet <- cbind(ysDet,forecastStep(IC=mean(initialXs),b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
-                                            n=1,NT=length(days),Tair=dataFinal$TairMu[,i],D=dataFinal$D[,i]))
-          ysParam <- cbind(ysParam,forecastStep(IC=mean(initialXs),b0=b0[prow],b1=b1[prow],b2=b2[prow],b3=b3[prow],b4=b4[prow],b5=b5[prow],
-                                                n=Nmc,NT=NT,Tair=dataFinal$TairMu[,i],D=dataFinal$D[,i]))
-          ysProc <- cbind(ysProc,forecastStep(IC=mean(initialXs),b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
-                                              n=Nmc,NT=NT,Q=sqrt(1/out.mat$p.proc[prow]),Tair=dataFinal$TairMu[,i],D=dataFinal$D[,i]))
-          ysIC <- cbind(ysIC,forecastStep(IC=initialXs,b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
-                                          n=length(initialXs),NT=NT,Tair=dataFinal$TairMu[,i],D=dataFinal$D[,i]))
-        }
-        N.IP.ci = apply(ysParam,2,quantile,c(0.025,0.5,0.975))
-        N.Proc.ci = apply(ysProc,2,quantile,c(0.025,0.5,0.975))
-        N.IC.ci = apply(ysIC,2,quantile,c(0.025,0.5,0.975))
-        predictedValues <- list(ysDet=ysDet,N.IP.ci=N.IP.ci,N.Proc.ci=N.Proc.ci,N.IC.ci=N.IC.ci)
         outFileName <- paste0("hindcasts/",siteName,"_",calibrationSiteName,"_parameters_",tID,"_",b,"_hindcasts.RData")
-        save(file=outFileName,predictedValues)
+        if(!file.exists(outFileName)){
+          
+          load(fileName) #Load Model Output 
+          
+          out.mat <- data.frame(as.matrix(out.burn$param))
+          
+          b0 <- out.mat$b0
+          b1 <- out.mat$b1
+          b2 <- out.mat$b2
+          if(b=="b3"){
+            b3 <- out.mat$b3
+            b4 <- rep(0,length(b3))
+            b5 <- rep(0,length(b3))
+          }else if(b=="b4"){
+            b4 <- out.mat$b4
+            b3 <- rep(0,length(b4))
+            b5 <- rep(0,length(b4))
+          }else if(b=="b5"){
+            b5 <- out.mat$b5
+            b3 <- rep(0,length(b5))
+            b4 <- rep(0,length(b5))
+          }
+          
+          NT <- length(dataFinal$TairMu[,1])
+          
+          Nmc <- 1000
+          prow = sample.int(nrow(out.mat),Nmc,replace=TRUE)
+          
+          initialXs <- rbeta(prow,dataFinal$x1.a[1],dataFinal$x1.b[1])
+          
+          print(mean(initialXs))
+          
+          days <- seq(1,NT)
+          ysDet <- forecastStep(IC=mean(initialXs),b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
+                                n=1,NT=length(days),Tair=dataFinal$TairMu[,1],D=dataFinal$D[,1]) #Dependent on IC, but independent for >=1
+          ysParam <- forecastStep(IC=mean(initialXs),b0=b0[prow],b1=b1[prow],b2=b2[prow],b3=b3[prow],b4=b4[prow],b5=b5[prow],
+                                  n=Nmc,NT=NT,Tair=dataFinal$TairMu[,1],D=dataFinal$D[,1])
+          ysProc <- forecastStep(IC=mean(initialXs),b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
+                                 n=Nmc,NT=NT,Q=sqrt(1/out.mat$p.proc[prow]),Tair=dataFinal$TairMu[,1],D=dataFinal$D[,1])
+          ysIC <- forecastStep(IC=initialXs,b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
+                               n=length(initialXs),NT=NT,Tair=dataFinal$TairMu[,1],D=dataFinal$D[,1])
+          if(dataFinal$N>1){
+            for(i in 2:dataFinal$N){
+              initialXs <- rbeta(prow,dataFinal$x1.a[i],dataFinal$x1.b[i])
+              
+              ysDet <- cbind(ysDet,forecastStep(IC=mean(initialXs),b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
+                                                n=1,NT=length(days),Tair=dataFinal$TairMu[,i],D=dataFinal$D[,i]))
+              ysParam <- cbind(ysParam,forecastStep(IC=mean(initialXs),b0=b0[prow],b1=b1[prow],b2=b2[prow],b3=b3[prow],b4=b4[prow],b5=b5[prow],
+                                                    n=Nmc,NT=NT,Tair=dataFinal$TairMu[,i],D=dataFinal$D[,i]))
+              ysProc <- cbind(ysProc,forecastStep(IC=mean(initialXs),b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
+                                                  n=Nmc,NT=NT,Q=sqrt(1/out.mat$p.proc[prow]),Tair=dataFinal$TairMu[,i],D=dataFinal$D[,i]))
+              ysIC <- cbind(ysIC,forecastStep(IC=initialXs,b0=mean(b0),b1=mean(b1),b2=mean(b2),b3=mean(b3),b4=mean(b4),b5=mean(b5),
+                                              n=length(initialXs),NT=NT,Tair=dataFinal$TairMu[,i],D=dataFinal$D[,i]))
+            }
+          }
+          N.IP.ci = apply(ysParam,2,quantile,c(0.025,0.5,0.975))
+          N.Proc.ci = apply(ysProc,2,quantile,c(0.025,0.5,0.975))
+          N.IC.ci = apply(ysIC,2,quantile,c(0.025,0.5,0.975))
+          predictedValues <- list(ysDet=ysDet,N.IP.ci=N.IP.ci,N.Proc.ci=N.Proc.ci,N.IC.ci=N.IC.ci)
+          save(file=outFileName,predictedValues)
+        }
       }
     }
   }
